92219d614c6540c8b80a4e73ae8b85b8b7083dfe
Pedro Pimenta <pedro@pimenta.co>
2022-03-15 12:35:16 +0100

    Cleanup All Maps

--- front-end/src/containers/all-maps/component.tsx
+++ front-end/src/containers/all-maps/component.tsx
@@ -1,18 +1,6 @@
-import { FC, useCallback, useMemo, useEffect, useState, useRef } from 'react';
+import { FC } from 'react';
 
-import Button from 'components/button';
-import MapSlider from 'components/map-slider';
-import MapVisualization from 'containers/map-visualization';
-
-import ElRiesgoClimatico from 'containers/sections/el-riesgo-climatico';
 import ElRiesgoClimaticoMap from 'containers/sections/el-riesgo-climatico/map';
-
-import {
-  TITLE,
-  SCENARIOS,
-  YEARS_CALENTAMIENTO,
-  YEARS_SEQUIAS,
-} from 'containers/sections/el-riesgo-climatico/constants';
 
 const AllMaps: FC = () => {
   return (
