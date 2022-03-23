2cd2be970ec53c62bf3a5a60367b9585bb15ad6f
Pedro Pimenta <pedro@pimenta.co>
2022-03-23 20:10:59 +0100

    Remove all maps page

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 // import queryString from 'query-string';
 
 // containers
@@ -8,7 +7,6 @@ import Hero from 'containers/sections/hero';
 // import MapVisualization from 'containers/map-visualization';
 import CultivoCulturaCambio from 'containers/sections/cultivo-cultura-cambio';
 import ElRiesgoClimatico from 'containers/sections/el-riesgo-climatico';
-import AllMaps from 'containers/all-maps';
 // import DesafioGlobalHeadline from 'containers/sections/desafio-global/headline';
 // import DesafioGlobalNarrative from 'containers/sections/desafio-global/narrative';
 // import EconomiaEnRiesgoHeadline from 'containers/sections/economia-en-riesgo/headline';
@@ -18,27 +16,19 @@ import Menu from 'containers/menu';
 import { SectionIDs } from 'types';
 
 const Welcome: React.FC = () => {
-  const [isMaps, setIsMaps] = useState(false);
-  // const [activeLayerId, setActiveLayerId] = useState();
-  // const query = location.search;
-  useEffect(() => {
-    setIsMaps(location.search.indexOf('mapas') >= 0);
-  }, []);
-
   return (
     <div>
-      {!isMaps && (
-        <div>
-          <Menu />
-          {/* <Headline /> */}
-          <Hero />
-          <Section id={SectionIDs.CultivoCulturaCambio} title="Cultivo, cultura, cambio">
-            <CultivoCulturaCambio />
-          </Section>
-          <Section id={SectionIDs.ElRiesgoClimatico} title="El Riesgo Climático">
-            <ElRiesgoClimatico />
-          </Section>
-          {/* <Section id={SectionIDs.EconomiaRiesgo} title="Section 2">
+      <div>
+        <Menu />
+        {/* <Headline /> */}
+        <Hero />
+        <Section id={SectionIDs.CultivoCulturaCambio} title="Cultivo, cultura, cambio">
+          <CultivoCulturaCambio />
+        </Section>
+        <Section id={SectionIDs.ElRiesgoClimatico} title="El Riesgo Climático">
+          <ElRiesgoClimatico />
+        </Section>
+        {/* <Section id={SectionIDs.EconomiaRiesgo} title="Section 2">
             <EconomiaEnRiesgoHeadline />
           </Section>
           <Section id={SectionIDs.EfectosCultivos} title="Section 3">
@@ -47,9 +37,7 @@ const Welcome: React.FC = () => {
           <Section id={SectionIDs.Conclusiones} title="Section 4">
             <ConclusionesHeadline />
           </Section> */}
-        </div>
-      )}
-      {isMaps && <AllMaps />}
+      </div>
     </div>
   );
 };
