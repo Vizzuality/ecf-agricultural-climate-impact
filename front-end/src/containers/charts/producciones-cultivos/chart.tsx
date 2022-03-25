36f6cb84dbd0e3cd40183158718e913f2424aa0d
David Inga <david.inga@vizzuality.com>
2022-03-25 16:33:00 +0100

    line charts

--- front-end/src/containers/charts/producciones-cultivos/chart.tsx
+++ front-end/src/containers/charts/producciones-cultivos/chart.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useMemo } from 'react';
-import { Line, AreaStack, Area, AreaClosed, Stack } from '@visx/shape';
+import { Line, LinePath } from '@visx/shape';
 import { scaleLinear } from '@visx/scale';
 import { Group } from '@visx/group';
 import { AxisLeft, AxisBottom } from '@visx/axis';
@@ -12,7 +12,7 @@ import {
 } from '@visx/tooltip';
 import { localPoint } from '@visx/event';
 import { GlyphCircle } from '@visx/glyph';
-import { extent, bisector, group, max } from 'd3-array';
+import { extent, bisector } from 'd3-array';
 
 import { DatasetItem, useProduccionesCultivos } from 'hooks/charts';
 
@@ -27,23 +27,23 @@ const bisectDate = bisector<DatasetItem, number>((d: DatasetItem) => d?.year).le
 // Format date for axis
 const formatDate = (year: number) => year.toString();
 
+const margin = { top: 40, right: 100, bottom: 50, left: 85 };
+
 const LABELS_BY_INDICATOR = {
   'Producción de cereales grano': 'Cereal',
   'Producción de aceituna': 'Aceituna',
   'Producción de uva': 'Uva',
 };
 
-const COLORS = ['red', 'green', 'yellow'];
-
-const margin = { top: 40, right: 100, bottom: 50, left: 85 };
+const COLORS = ['#B23E3E', '#A1B57D', '#F9F5DB'];
 
 export const Chart: React.FC<ChartProps> = ({ width, height }) => {
-  const [cerealData, aceitunaData, uvaData] = useProduccionesCultivos();
-  const isFetching = cerealData.isFetching || aceitunaData.isFetching || uvaData.isFetching;
+  const [cereales, aceituna, uva] = useProduccionesCultivos();
+  const isFetching = cereales.isFetching || aceituna.isFetching || uva.isFetching;
 
   const lastPoints = useMemo(
-    () => [cerealData.data, aceitunaData.data, uvaData.data].map((data) => data[data.length - 1]),
-    [cerealData.data, aceitunaData.data, uvaData.data],
+    () => [cereales.data, aceituna.data, uva.data].map((data) => data[data.length - 1]),
+    [cereales.data, aceituna.data, uva.data],
   );
 
   // size adjustments
@@ -52,30 +52,9 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
 
   // Whole data
   const concatData = useMemo(
-    () => cerealData.data.concat(aceitunaData.data).concat(uvaData.data),
-    [cerealData.data, uvaData.data, aceitunaData.data],
+    () => cereales.data.concat(aceituna.data).concat(uva.data),
+    [cereales.data, aceituna.data, uva.data],
   );
-
-  const stackedData = useMemo(() => {
-    const result = Array.from(
-      group(concatData, (d) => d.year),
-      ([key, value]) => {
-        const data = value
-          .sort((a, b) => a.value - b.value)
-          .map(({ indicator, value }) => ({ [indicator]: value }))
-          .reduce((a, b) => ({ ...(a || {}), ...b }));
-        const total = value.map(({ value }) => value).reduce((a, b) => a + b);
-        return {
-          year: key,
-          ...data,
-          data: value.sort((a, b) => -(a.value - b.value)),
-          total,
-        };
-      },
-    );
-    console.log(result);
-    return result;
-  }, [concatData]);
 
   const getDataByYear = useCallback(
     (data: DatasetItem) => concatData.filter((d) => d.year === data.year),
@@ -88,9 +67,10 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
     () =>
       scaleLinear({
         range: [0, innerWidth],
-        domain: extent(stackedData, (d) => d.year),
+        domain: extent(concatData, getYear),
+        clamp: true,
       }),
-    [innerWidth, stackedData],
+    [concatData, innerWidth],
   );
 
   // vertical, y scale
@@ -98,9 +78,10 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
     () =>
       scaleLinear({
         range: [innerHeight, 0],
-        domain: [0, max(stackedData, (d) => d.total)],
+        domain: extent(concatData, getValue),
+        clamp: true,
       }),
-    [innerHeight, stackedData],
+    [concatData, innerHeight],
   );
 
   // tooltip
@@ -155,7 +136,7 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
             fill="white"
             className="font-bold"
           >
-            Tonnes
+            Temperature ºC
           </text>
           <AxisLeft
             hideAxisLine={true}
@@ -184,115 +165,29 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
               opacity: '0.5',
             })}
           />
-          {/* <Stack
-            data={stackedData}
-            keys={Object.keys(stackedData[0] || []).filter(
-              (key) => key !== 'year' && key !== 'total' && key !== 'data',
-            )}
-            offset="wiggle"
-            order="none"
-            // color={colorScale}
-            x={(d) => timeScale(d.data.year) ?? 0}
-            // x={(_, i) => timeScale(i) ?? 0}
-            y0={(d) => valueScale(d[0]) ?? 0}
-            y1={(d) => valueScale(d[1]) ?? 0}
-          >
-            {({ stacks, path }) => console.log(stacks) ||
-              stacks.map((stack, i) => {
-                // Alternatively use renderprops <Spring to={{ d }}>{tweened => ...}</Spring>
-                // const pathString = path(stack) || '';
-                // const tweened = animate ? useSpring({ pathString }) : { pathString };
-                // const color = colorScale(stack.key);
-                // const pattern = patternScale(stack.key);
-                return (
-                  <g key={`series-${stack.key}`}>
-                    <path d={path(stack) || ''} stroke="transparent" fill={COLORS[i]} />
-                  </g>
-                );
-              })
-            }
-          </Stack> */}
-          <Area
-            data={stackedData}
-            x={(d) => timeScale(d.year) ?? 0}
-            y0={(d) => valueScale(d['Producción de cereales grano']) ?? 0}
-            y1={(d) =>
-              valueScale(
-                d['Producción de cereales grano'] -
-                  d['Producción de uva'] -
-                  d['Producción de aceituna'],
-              ) ?? 0
-            }
-            fill={COLORS[0]}
-            opacity={0.6}
-          />
-
-          <Area
-            data={stackedData}
-            x={(d) => timeScale(d.year) ?? 0}
-            y0={(d) =>
-              valueScale(
-                d['Producción de cereales grano'] -
-                  d['Producción de uva'] -
-                  d['Producción de aceituna'],
-              ) ?? 0
-            }
-            y1={(d) => valueScale(d['Producción de uva'] + d['Producción de aceituna']) ?? 0}
-            fill={COLORS[2]}
-            opacity={0.6}
-          />
-
-          <Area
-            data={stackedData}
-            x={(d) => timeScale(d.year) ?? 0}
-            y0={(d) => valueScale(d['Producción de uva']) ?? 0}
-            y1={(d) => valueScale(d['Producción de uva'] - d['Producción de aceituna']) ?? 0}
-            fill={COLORS[1]}
-            opacity={0.6}
-          />
-
-          {/* <AreaStack
-            data={stackedData}
-            keys={Object.keys(stackedData[0] || []).filter((key) => key !== 'year')}
-            order="ascending"
-            x={(d) => timeScale(getYear(d.data)) ?? 0}
-            y0={(d) => valueScale(d[0]) ?? 0}
-            y1={(d) => valueScale(d[1]) ?? 0}
-          >
-            {({ stacks, path }) =>
-              stacks.map((stack, index) => (
-                <path
-                  key={`stack-${stack.key}`}
-                  d={path(stack) || ''}
-                  stroke="transparent"
-                  fill={COLORS[index]}
-                />
-              ))
-            }
-          </AreaStack> */}
-          {/* <LinePath
-            stroke="white"
+          <LinePath
+            stroke={COLORS[0]}
             strokeWidth={2}
-            data={cerealData.data}
+            data={cereales.data}
             x={(d) => timeScale(getYear(d)) ?? 0}
             y={(d) => valueScale(getValue(d)) ?? 0}
           />
           <LinePath
             key={'line-wew'}
-            stroke="white"
+            stroke={COLORS[1]}
             strokeWidth={2}
-            data={aceitunaData.data}
+            data={aceituna.data}
             x={(d) => timeScale(getYear(d)) ?? 0}
             y={(d) => valueScale(getValue(d)) ?? 0}
           />
           <LinePath
-            key={'line-waw'}
-            stroke="white"
+            key={'line-wew'}
+            stroke={COLORS[2]}
             strokeWidth={2}
-            data={uvaData.data}
+            data={uva.data}
             x={(d) => timeScale(getYear(d)) ?? 0}
             y={(d) => valueScale(getValue(d)) ?? 0}
-          /> */}
+          />
           {lastPoints.map((d, i) => {
             const w = 8;
             const x = timeScale(getYear(d)) - w / 2 ?? 0;
@@ -342,7 +237,7 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
                 />
               </g>
             ))}
-          {/* <rect
+          <rect
             width={innerWidth}
             height={innerHeight}
             x={0}
@@ -351,7 +246,7 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
             onMouseMove={handleTooltip}
             onMouseLeave={hideTooltip}
             fill="transparent"
-          /> */}
+          />
         </Group>
       </svg>
       {/* Tooltip: pop-up */}
@@ -381,7 +276,7 @@ export const Chart: React.FC<ChartProps> = ({ width, height }) => {
           }} /> */}
           {tooltipData.map((d) => (
             <div className="font-bold" key={`tooltip-item-${Math.random()}`}>
-              {LABELS_BY_INDICATOR[d.indicator]}: {d.value} {d.unit}
+              <span>{LABELS_BY_INDICATOR[d.indicator]}</span>: {d.value} {d.unit}
             </div>
           ))}
         </TooltipWithBounds>
