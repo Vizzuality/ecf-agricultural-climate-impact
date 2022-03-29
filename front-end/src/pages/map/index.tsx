492c57a956bff8f0eb901ebb0ef9e92a98d4bd7a
Pedro Pimenta <pedro@pimenta.co>
2022-03-29 12:18:51 +0200

    Divide map index pages into categories and reorder

--- front-end/src/pages/map/index.tsx
+++ front-end/src/pages/map/index.tsx
@@ -1,34 +1,60 @@
 const PAGES = [
-  { href: 'map/aridez/', label: 'Aridez' },
-  { href: 'map/calentamiento/', label: 'Proyecciones de calentamiento' },
-  // { href: 'map/cultivo-olivo/', label: 'Olivar' },
+  { href: 'map/calentamiento/', label: 'Proyecciones de calentamiento en España' },
+  { href: 'map/sequias/', label: 'Duración de las sequías a lo largo del año' },
   { href: 'map/cultivos/', label: 'Superficie destinada a los cultivos' },
-  { href: 'map/incendios-dehesa/', label: 'Riesgo de incendios en zonas de dehesa' },
-  { href: 'map/precipitacion/', label: 'Precipitación trimestre más húmedo' },
+  { href: 'map/zonas-optimas-olivo/', label: 'Cambio en zonas óptimas para el cultivo de olivo' },
+  { href: 'map/rendimiento-olivo/', label: 'Proyecciones de rendimiento del olivo' },
   {
     href: 'map/rendimiento-cereales/',
     label: 'Proyecciones de rendimiento del cultivo del cereal',
   },
-  { href: 'map/rendimiento-olivo/', label: 'Proyecciones de rendimiento del olivo' },
-  { href: 'map/sequias/', label: 'Proyecciones de sequías' },
-  {
-    href: 'map/sequias-dehesa/',
-    label: 'Duración de las sequías a lo largo del año en zonas de dehesa',
-  },
-  { href: 'map/zonas-optimas-olivo/', label: 'Cambio en zonas óptimas para el cultivo de olivo' },
   {
     href: 'map/zonas-optimas-vino/',
     label: 'Cambios en zonas óptimas para la producción de vino de calidad',
   },
+  {
+    href: 'map/sequias-dehesa/',
+    label: 'Duración de las sequías a lo largo del año en zonas de dehesa',
+  },
+  { href: 'map/incendios-dehesa/', label: 'Riesgo de incendios en zonas de dehesa' },
+  { href: 'map/aridez/', label: 'Aridez' },
+  { href: 'map/precipitacion/', label: 'Precipitación trimestre más húmedo' },
 ];
 
 const Index: React.FC = () => (
   <div className="p-12">
-    {PAGES.map((page) => (
-      <a className="block p-1" key={page.href} href={page.href}>
-        {page.label}
-      </a>
-    ))}
+    <>
+      <h3>El riesgo climático</h3>
+      {PAGES.map((page, i) => {
+        if (i < 2) {
+          return (
+            <a className="block p-1" key={page.href} href={page.href}>
+              {page.label}
+            </a>
+          );
+        }
+      })}
+      <h3>El riesgo agrícola</h3>
+      {PAGES.map((page, i) => {
+        if (i >= 2 && i < 9) {
+          return (
+            <a className="block p-1" key={page.href} href={page.href}>
+              {page.label}
+            </a>
+          );
+        }
+      })}
+      <h3>Otros</h3>
+      {PAGES.map((page, i) => {
+        if (i >= 9) {
+          return (
+            <a className="block p-1" key={page.href} href={page.href}>
+              {page.label}
+            </a>
+          );
+        }
+      })}
+    </>
   </div>
 );
 
