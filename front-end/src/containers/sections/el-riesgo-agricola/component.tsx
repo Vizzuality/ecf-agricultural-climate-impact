92269032d27314ec1d33edd5578cad8896ae34c6
Pedro Pimenta <pedro@pimenta.co>
2022-04-03 09:38:32 +0200

    Cleanup

--- front-end/src/containers/sections/el-riesgo-agricola/component.tsx
+++ front-end/src/containers/sections/el-riesgo-agricola/component.tsx
@@ -16,18 +16,6 @@ export const ElRiesgoAgricola: FC = () => {
       <MediaContextProvider>
         <Desktop includeBiggerScreens>
           <div className="w-full bg-primary-red">
-            <div className="flex flex-col items-start w-full max-w-screen-lg pt-32 mx-auto bg-primary-red">
-              <h1 className="relative px-3 py-2 font-serif text-6xl transition-opacity duration-500 text-primary-red">
-                <span className="relative z-10">lo jiesjo ajrikola</span>
-              </h1>
-              <div className="py-16 text-white" style={{ columns: '2' }}>
-                <p>blabla</p>
-                <p>blabla</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-start w-full max-w-screen-xl pb-32 mx-auto">
-              <div className="w-full">aki sharts</div>
-            </div>
             <div className="flex w-full pt-16">
               <ElRiesgoAgricolaMap />
             </div>
