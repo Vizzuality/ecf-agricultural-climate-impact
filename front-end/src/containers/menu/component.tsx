216159d61be5e00310b64bde972858c313d5bed4
Pedro Pimenta <pedro@pimenta.co>
2022-04-11 20:41:18 +0200

    Fix mobile menu

--- front-end/src/containers/menu/component.tsx
+++ front-end/src/containers/menu/component.tsx
@@ -73,49 +73,62 @@ export const Menu: FC = () => {
         </Desktop>
         <Mobile>
           <Headroom>
-            <div className="flex justify-between w-full p-2 mx-auto overflow-hidden bg-white backdrop-blur-sm backdrop-filter bg-opacity-90 max-w-screen">
+            <div className="flex justify-between w-full max-w-full p-2 mx-auto overflow-x-hidden bg-white backdrop-blur-sm backdrop-filter bg-opacity-90">
               <div className="relative flex items-center top-2">
                 <a href="#section-hero">
                   <img className="w-24" src="images/logo-coag.png" alt="COAG" />
                 </a>
               </div>
               <div className="flex items-center">
-                <Button theme="none" size="sm" onClick={() => handleSideMenu()}>
+                <Button theme="none" size="s" onClick={() => handleSideMenu()}>
                   <Icon icon={sideMenuOpen ? MENU_CLOSE_SVG : MENU_SVG}></Icon>
                 </Button>
               </div>
-              <div
-                className={cx({
-                  'absolute top-full transition-all px-4 w-full bg-white backdrop-blur-sm backdrop-filter bg-opacity-90 items-center h-screen font-sans font-normal':
-                    true,
-                  'left-0': sideMenuOpen,
-                  'left-full': !sideMenuOpen,
-                })}
-                onClick={() => setSideMenuOpen(false)}
-              >
-                <ul className="flex flex-col gap-6 pt-4">
-                  {SECTIONS.map((section) => (
-                    <li key={`menu-item-${section.id}`}>
-                      <Link href={section.url}>
-                        <a className="opacity-100">{section.label}</a>
-                      </Link>
-                      {section?.subsections?.length && (
-                        <ul className="flex flex-col gap-6 px-6 pt-4">
-                          {section.subsections.map((subSection) => (
-                            <li key={`menu-item-${subSection.id}`}>
-                              <Link href={subSection.url}>
-                                <a className="opacity-100">{subSection.label}</a>
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </div>
             </div>
           </Headroom>
+          <div
+            className={cx({
+              'fixed top-0 transition-all px-2 w-full bg-white backdrop-blur-sm backdrop-filter bg-opacity-90 items-center h-screen font-sans font-normal':
+                true,
+              'left-0': sideMenuOpen,
+              'left-full': !sideMenuOpen,
+            })}
+            onClick={() => setSideMenuOpen(false)}
+            style={{ zIndex: 999 }}
+          >
+            <div className="flex justify-between w-full max-w-full py-2 mx-auto overflow-x-hidden bg-white backdrop-blur-sm backdrop-filter bg-opacity-90">
+              <div className="relative flex items-center top-2">
+                <a href="#section-hero">
+                  <img className="w-24" src="images/logo-coag.png" alt="COAG" />
+                </a>
+              </div>
+              <div className="flex items-center">
+                <Button theme="none" size="s" onClick={() => handleSideMenu()}>
+                  <Icon icon={sideMenuOpen ? MENU_CLOSE_SVG : MENU_SVG}></Icon>
+                </Button>
+              </div>
+            </div>
+            <ul className="flex flex-col gap-6 px-2 pt-4">
+              {SECTIONS.map((section) => (
+                <li key={`menu-item-${section.id}`}>
+                  <Link href={section.url}>
+                    <a className="opacity-100">{section.label}</a>
+                  </Link>
+                  {section?.subsections?.length && (
+                    <ul className="flex flex-col gap-6 px-6 pt-4">
+                      {section.subsections.map((subSection) => (
+                        <li key={`menu-item-${subSection.id}`}>
+                          <Link href={subSection.url}>
+                            <a className="opacity-100">{subSection.label}</a>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
         </Mobile>
       </MediaContextProvider>
     </div>
